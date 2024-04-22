bde14adb28d2a5d99c35bd342ce127e0c5912c46
Andrea Montalto <andrea87330@gmail.com>
2024-04-22 12:59:36 +0200

    Adding comments for sections

--- Bachelor_project_code.R
+++ Bachelor_project_code.R
@@ -3,9 +3,11 @@ library(readxl)
 data1 <- read_xlsx('capstone_airline_reviews3.xlsx')
 data2 <- read.csv('Dataset2.csv')
 
-<<<<<<< HEAD
 
-y <- 5
-=======
+## DATA cleaning ## 
+
+## Data formatting
+
+
 #Github has the worst user experience ever
->>>>>>> f485992869305ef01ba128ee819c2833997505f3
+
