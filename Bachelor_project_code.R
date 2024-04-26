bc641b01fe9f1589b24dfe6ecd3d750002c8d1ac
AndreaMontalto <andrea87330@gmail.com>
2024-04-26 12:21:28 +0200

    First lines of code dataset 2

--- Bachelor_project_code.R
+++ Bachelor_project_code.R
@@ -6,7 +6,17 @@ data2 <- read.csv('Dataset2.csv')
 
 ## DATA cleaning ## 
 
+# Working on dataset 2# 
+
+#removing uncessary variables 
+
+data2 <- subset(data2, select = -c(X, Airline.Name, Route, Wifi...Connectivity)) 
+str(data2)
+#Checking for missing values 
+NAs_data2 <- sum(is.na(data2))
+
+apply()
 ## Data formatting
 
-#editing code
+
 
